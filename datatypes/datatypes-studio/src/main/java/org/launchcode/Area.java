ec38304e29422da160b16ae70623dcc747d522a7
bmm853 <rebeccawnelson@gmail.com>
2023-08-21 20:08:12 -0500

    Completed bonus mission

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -5,8 +5,21 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         System.out.println("Enter a radius: ");
+
+//        while(!input.hasNextDouble()) {
+//            System.out.println("Enter a valid number for the radius : ");
+//            input.next();
+//        }
+
         double radius = input.nextDouble();
+
+        while (radius <= 0) {
+            System.out.println("Enter a valid number for the radius : ");
+            radius = input.nextDouble();
+        }
+
         input.close();
 
 //        double area = Math.PI * radius * radius;
