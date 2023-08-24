a55bd66240ff1b295355f7a5c38d6fe9113e8d52
bmm853 <rebeccawnelson@gmail.com>
2023-08-24 12:59:04 -0500

    Completed exercises

---
+++ control-flow-and-collections/exercises/src/main/java/org/launchcode/ArrayListPractice.java
@@ -0,0 +1,58 @@
+package org.launchcode;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ArrayListPractice {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int total = 0;
+        ArrayList<Integer> nums = new ArrayList<>();
+        ArrayList<String> wordArr = new ArrayList<>();
+
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+        nums.add(5);
+        nums.add(6);
+        nums.add(7);
+        nums.add(8);
+        nums.add(9);
+        nums.add(10);
+
+        wordArr.add("fuzzy");
+        wordArr.add("seventeen");
+        wordArr.add("jar");
+        wordArr.add("squid");
+        wordArr.add("world");
+
+        System.out.println(getEvenSum(nums));
+
+        System.out.println("Search for words with how many letters? ");
+        int chars = input.nextInt();
+        input.close();
+        printWords(wordArr, chars);
+    }
+
+    public static int getEvenSum(ArrayList<Integer> numbers) {
+        int sum = 0;
+
+        for (int num: numbers) {
+            if (num % 2 == 0) {
+                sum +=num;
+            }
+        }
+
+        return sum;
+    }
+
+    public static void printWords(ArrayList<String> words, int numChars) {
+        for (String word : words) {
+            if (word.length() == numChars) {
+                System.out.println(word);
+            }
+        }
+    }
+}
---
+++ control-flow-and-collections/exercises/src/main/java/org/launchcode/HashMapPractice.java
@@ -0,0 +1,38 @@
+package org.launchcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class HashMapPractice {
+    public static void main(String[] args) {
+        HashMap<String, Integer> students = new HashMap<>();
+        Scanner input = new Scanner(System.in);
+        String newStudent;
+
+        //Get student name and ID
+        do {
+
+            System.out.print("Student: ");
+            newStudent = input.nextLine();
+
+            if (!newStudent.equals("")) {
+                System.out.print("Student ID: ");
+                Integer newID = input.nextInt();
+                students.put(newStudent, newID);
+
+                // Read in the newline before looping back
+                input.nextLine();
+            }
+
+        } while(!newStudent.equals(""));
+
+        System.out.println("\nClass roster:");
+
+        for (Map.Entry<String, Integer> student : students.entrySet()) {
+            System.out.println(student.getKey() + " (ID: " + student.getValue() + ")");
+        }
+
+        System.out.println("Number of students in roster: " + students.size());
+    }
+}
