e7dff2737b2cb3d83933116b4be8fbca021aa447
bmm853 <rebeccawnelson@gmail.com>
2023-09-17 15:33:14 -0500

    Completed exercise requirements

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -1,17 +1,64 @@
 package org.launchcode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        ArrayList<Topping> toppings = menu.getToppings();
+        Comparator comparator = new FlavorComparator();
+        Comparator coneComparator = new ConeComparator();
+        Comparator toppingComparator = new ToppingComparator();
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
 
+//        for (Flavor flavor : flavors) {
+//            System.out.println(flavor.getName());
+//        }
+
+        for (Flavor flavor : flavors) {
+            System.out.println(flavor.getName() + ": " + flavor.getAllergens().size() + " allergen(s)");
+        }
+
+        flavors.sort(comparator);
+
+        System.out.println("\n");
+
+        for (Flavor flavor : flavors) {
+            System.out.println(flavor.getName() + ": " + flavor.getAllergens().size() + " allergen(s)");
+        }
+
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
+        System.out.println("\n");
+
+        for (Cone cone : cones) {
+            System.out.println(cone.getName() + ": $" + cone.getCost());
+        }
+        cones.sort(coneComparator);
+
+        System.out.println("\n");
+
+        for (Cone cone : cones) {
+            System.out.println(cone.getName() + ": $" + cone.getCost());
+        }
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+
+        System.out.println("\n");
+
+        for (Topping topping : toppings) {
+            System.out.println(topping.getName());
+        }
+
+        toppings.sort(toppingComparator);
+
+        System.out.println("\n");
+
+        for (Topping topping : toppings) {
+            System.out.println(topping.getName());
+        }
     }
 }
